107b70a407861fd04929c73a0d1a398f050928a7
Satvik <satvikchoudhary@gmail.com>
2021-12-30 16:37:29 +0530

    added image

--- frontend/pages/api/hello.tsx
+++ frontend/pages/api/hello.tsx
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function helloAPI(req, res) {
+export default function helloAPI(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({ name: 'John Doe' })
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -28,48 +28,26 @@ export default function Home() {
           wo Legenden geboren werden
         </p>
 
-        {/*<div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">*/}
+        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
 
-        {/*  <a*/}
-        {/*    href="https://nextjs.org/docs"*/}
-        {/*    className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"*/}
-        {/*  >*/}
-        {/*    <h3 className="text-2xl font-bold">Documentation &rarr;</h3>*/}
-        {/*    <p className="mt-4 text-xl">*/}
-        {/*      Find in-depth information about Next.js features and API.*/}
-        {/*    </p>*/}
-        {/*  </a>*/}
+          <div className="flex: 1; display: flex;
+/* content styling OPTIONAL*/ align-items: center; justify-content: center; padding: 10vh;
+/* background styling */ background-size: cover; background-position: center; background-repeat: no-repeat;
+-webkit-flex-wrap: nowrap; flex-wrap: nowrap;">
+            <Image className="position: relative w-full background-size: cover"
+               width={1920}
+               height={1920}
+               src="/chess-board.jpg"
+               alt="Schach HD"
+            />
 
-        {/*  <a*/}
-        {/*    href="https://nextjs.org/learn"*/}
-        {/*    className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"*/}
-        {/*  >*/}
-        {/*    <h3 className="text-2xl font-bold">Learn &rarr;</h3>*/}
-        {/*    <p className="mt-4 text-xl">*/}
-        {/*      Learn about Next.js in an interactive course with quizzes!*/}
-        {/*    </p>*/}
-        {/*  </a>*/}
+            <div className="">
+              Hello World
+            </div>
+          </div>
 
-        {/*  <a*/}
-        {/*    href="https://github.com/vercel/next.js/tree/master/examples"*/}
-        {/*    className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"*/}
-        {/*  >*/}
-        {/*    <h3 className="text-2xl font-bold">Examples &rarr;</h3>*/}
-        {/*    <p className="mt-4 text-xl">*/}
-        {/*      Discover and deploy boilerplate example Next.js projects.*/}
-        {/*    </p>*/}
-        {/*  </a>*/}
 
-        {/*  <a*/}
-        {/*    href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"*/}
-        {/*    className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"*/}
-        {/*  >*/}
-        {/*    <h3 className="text-2xl font-bold">Deploy &rarr;</h3>*/}
-        {/*    <p className="mt-4 text-xl">*/}
-        {/*      Instantly deploy your Next.js site to a public URL with Vercel.*/}
-        {/*    </p>*/}
-        {/*  </a>*/}
-        {/*</div>*/}
+        </div>
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
