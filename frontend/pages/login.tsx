0b75aaac630586da8f59b3476ce9eb47d462712a
Satvik <satvikchoudhary@gmail.com>
2021-12-30 20:19:11 +0530

    types for login/signup

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,7 +1,20 @@
+import React from 'react'
 
-/** Class to handle the login page with google and facebook login api integration. */
-export default class Login {
-  render() {
-
+/** Class to handle the login. */
+export default class Login extends React.Component<{}, {}> {
+  /**
+   * Render the login page.
+   * @returns The rendered login page.
+   */
+  public render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <p>
+          <a href="/auth/google">Login with Google</a>
+        </p>
+      </div>
+    )
   }
 }
+
