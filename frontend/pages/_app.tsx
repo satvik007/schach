f2fab8bb26dab66537f29646158a79fbb317c9d8
Satvik <satvikchoudhary@gmail.com>
2021-12-31 13:20:41 +0530

    partial changes for login/signup

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app';
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
+// Use the <SessionProvider> to improve performance and allow components that call
+// `useSession()` anywhere in your application to access the `session` object.
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider
+      // Provider options are not required but can be useful in situations where
+      // you have a short session maxAge time. Shown here with default values.
+      session={pageProps.session}
+    >
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,20 +1,62 @@
-import React from 'react'
+import React from "react";
+// import { GetServerSideProps } from "next"
+// import type { Session } from "next-auth"
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Link from "next/link";
 
 /** Class to handle the login. */
-export default class Login extends React.Component<{}, {}> {
-  /**
-   * Render the login page.
-   * @returns The rendered login page.
-   */
-  public render() {
-    return (
-      <div>
-        <h1>Login</h1>
-        <p>
-          <a href="/auth/google">Login with Google</a>
-        </p>
-      </div>
-    )
-  }
+export default function Login() {
+  // As this page uses Server Side Rendering, the `session` will be already
+  // populated on render without needing to go through a loading stage.
+  // This is possible because of the shared context configured in `_app.js` that
+  // is used by `useSession()`.
+  const { data: session, status } = useSession();
+  // const router = useRouter();
+  return <div>Access Token: {!!session && session.accessToken || "null"}</div>
+
+  // return (
+  //   <div className="flex flex-col items-center justify-center min-h-screen py-2">
+  //     <Head>
+  //       <title>Anmeldung zum Schach</title>
+  //       <link rel="icon" href="/favicon.ico" />
+  //     </Head>
+  //
+  //     <main className="flex flex-col items-center w-full flex-1 px-20 text-center">
+  //       <header>
+  //         <noscript>
+  //           <style>{`.nojs-show { opacity: 1; top: 0; }`}</style>
+  //         </noscript>
+  //       </header>
+  //       <h1 className="text-4xl font-bold">Anmeldung zum Schach</h1>
+  //
+  //
+  //
+  //       {/*<div className="flex flex-col items-center w-full flex-1 px-20 text-center">*/}
+  //
+  //       {/*</div>*/}
+  //     </main>
+  //   </div>
+  // );
+
+  //
+  // const {data: session, status} = useSession()
+  // const loading = status === 'loading'
+  // return (
+  //   <>
+  //
+  //   </>
+  // )
 }
 
+// Export the `session` prop to use sessions with Server Side Rendering
+// export const getServerSideProps: GetServerSideProps<{
+//   session: Session | null
+// }> = async (context) => {
+//   return {
+//     props: {
+//       session: await getSession(context),
+//     },
+//   }
+// }
