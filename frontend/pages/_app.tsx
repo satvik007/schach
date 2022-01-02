e2ed29b728e72c463134e6f9b8298faf1f295834
Satvik <satvikchoudhary@gmail.com>
2022-01-02 17:56:07 +0530

    added layout component, import for broke.css

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import "../styles/broke.css";
+import Layout from "../components/layout";
 
 // Use the <SessionProvider> to improve performance and allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
@@ -11,7 +13,9 @@ function MyApp({ Component, pageProps }: AppProps) {
       // you have a short session maxAge time. Shown here with default values.
       session={pageProps.session}
     >
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </SessionProvider>
   );
 }
