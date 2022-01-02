836da017c849ef5db520c15bb214b263b3a68797
Satvik <satvikchoudhary@gmail.com>
2022-01-02 17:52:41 +0530

    failed attempt at adding twitter oauth

--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,14 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+// import TwitterProvider from "next-auth/providers/twitter"
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
 import clientPromise from "../../../lib/mongodb"
+
+const {GITHUB_ID, GITHUB_SECRET, TWITTER_ID, TWITTER_SECRET} = process.env;
+
+if (!GITHUB_ID || !GITHUB_SECRET || !TWITTER_ID || !TWITTER_SECRET) {
+  throw new Error("Missing environment variables")
+}
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
@@ -9,9 +16,13 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: GITHUB_ID,
+      clientSecret: GITHUB_SECRET,
     }),
+    // TwitterProvider({
+    //   clientId: TWITTER_ID,
+    //   clientSecret: TWITTER_SECRET
+    // })
     // ...add more providers here
   ],
   secret: process.env.SECRET,
